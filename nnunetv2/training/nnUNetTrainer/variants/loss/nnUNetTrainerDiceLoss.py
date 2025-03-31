f2a58b4c699b3645b25d1ed6270a8118b9f22921
Thomas Dagonneau <thdaga@romane>
2025-03-31 14:47:25 -0400

    added nnUNetTrainerDiceCELoss_noSmooth_300epochs to run training only for 300 epochs

--- nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerDiceLoss.py
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerDiceLoss.py
@@ -69,3 +69,10 @@ class nnUNetTrainerDiceCELoss_noSmooth_2000epochs(nnUNetTrainerDiceCELoss_noSmoo
                  device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
         self.num_epochs = 2000
+
+
+class nnUNetTrainerDiceCELoss_noSmooth_300epochs(nnUNetTrainerDiceCELoss_noSmooth):
+    def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.num_epochs = 300
